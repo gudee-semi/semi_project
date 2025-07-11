374e1cd49393242bdf1107a9848d6a91e21bfb45
kimhyewon92 <sing92@naver.com>
2025-07-11 09:25:49 +0900

    paging
    paging

--- library_lms/src/main/java/com/hy/controller/qna/QnaViewServlet.java
+++ library_lms/src/main/java/com/hy/controller/qna/QnaViewServlet.java
@@ -1,22 +1,63 @@
 package com.hy.controller.qna;
 
+import java.io.IOException;
+import java.util.List;
+
+import com.hy.dto.Qna;
+import com.hy.service.QnaService;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @WebServlet("/qna/view")
 public class QnaViewServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private QnaService service = new QnaService();
        
     public QnaViewServlet() {
         super();
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("/views/qna/qnaPage").forward(request, response);
+		// 로그인이 안된 사람은 처음 메인페이지로 가게되어있음 현재 로그인페이지가 index가 아니어서
+//		HttpSession session = request.getSession(false);
+//		if(session == null) {
+//			response.sendRedirect(request.getContextPath()+"/");
+//			return;
+//		} else {
+//			if(session.getAttribute("loginMember") == null) {
+//				response.sendRedirect(request.getContextPath()+"/");
+//				return;
+//			}
+//		}
+		
+		Qna param = new Qna();
+		// 현재 페이지 정보 셋팅
+		int nowPage = 1;
+		String nowPageStr = request.getParameter("nowPage");
+		if(nowPageStr != null) {
+			nowPage = Integer.parseInt(nowPageStr);
+		}
+		param.setNowPage(nowPage);
+		
+		// 검색어 셋팅
+		String keyword = request.getParameter("keyword");
+		param.setKeyword(keyword);
+		
+		// 전체 게시글 개수 조회
+		int totalData = service.selectQnaCount(param);
+		param.setTotalData(totalData);
+		
+		// 게시글 목록 정보 조회
+		List<Qna> qnaList = service.selectQnaList(param);
+		
+		request.setAttribute("paging", param);
+		request.setAttribute("qnaList", qnaList);
+		request.getRequestDispatcher("/views/qna/list.jsp").forward(request, response);
+		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- library_lms/src/main/java/com/hy/dao/QnaDao.java
+++ library_lms/src/main/java/com/hy/dao/QnaDao.java
@@ -11,46 +11,46 @@ import com.hy.dto.Qna;
 public class QnaDao {
 	public List<Qna> selectQnaList(Qna param){
 		SqlSession session = SqlSessionTemplate.getSqlSession(true);
-		List<Qna> list = session.selectList("com.gn.mapper.QnaMapper.selectQnaList", param);
+		List<Qna> list = session.selectList("com.hy.mapper.QnaMapper.selectQnaList", param);
 		session.close();
 		return list;
 	}
 	
+	public int selectQnaCount(Qna param) {
+		SqlSession session = SqlSessionTemplate.getSqlSession(true);
+		int count = session.selectOne("com.hy.mapper.QnaMapper.selectQnaCount", param);
+		session.close();
+		return count;
+	}
+	
 	public Qna selectQnaOne(int qnaNo) {
 		SqlSession session = SqlSessionTemplate.getSqlSession(true);
-		Qna qna = session.selectOne("com.gn.mapper.QnaMapper.selectQnaOne", qnaNo);
+		Qna qna = session.selectOne("com.hy.mapper.QnaMapper.selectQnaOne", qnaNo);
 		session.close();
 		return qna;
 	}
 	
 	public Attach selectAttachByQnaNo(int qnaNo) {
 		SqlSession session = SqlSessionTemplate.getSqlSession(true);
-		Attach attach = session.selectOne("com.gn.mapper.QnaMapper.selectAttachByQnaNo", qnaNo);
+		Attach attach = session.selectOne("com.hy.mapper.QnaMapper.selectAttachByQnaNo", qnaNo);
 		session.close();
 		return attach;
 	}
 	
 	public Attach selectAttachByAttachNo(int attachNo) {
 		SqlSession session = SqlSessionTemplate.getSqlSession(true);
-		Attach attach = session.selectOne("com.gn.mapper.QnaMapper.selectAttachByAttachNo", attachNo);
+		Attach attach = session.selectOne("com.hy.mapper.QnaMapper.selectAttachByAttachNo", attachNo);
 		session.close();
 		return attach;
 	}
 	
-	public int selectQnaCount(Qna param) {
-		SqlSession session = SqlSessionTemplate.getSqlSession(true);
-		int count = session.selectOne("com.gn.mapper.QnaMapper.selectQnaCount", param);
-		session.close();
-		return count;
-	}
-	
 	public int insertQna(SqlSession session, Qna qna) {
-		int result = session.insert("com.gn.mapper.QnaMapper.insertQna", qna);
+		int result = session.insert("com.hy.mapper.QnaMapper.insertQna", qna);
 		return result;
 	}
 	
 	public int insertAttach(SqlSession session, Attach attach) {
-		int result = session.insert("com.gn.mapper.QnaMapper.insertAttach", attach);
+		int result = session.insert("com.hy.mapper.QnaMapper.insertAttach", attach);
 		return result;
 	}
 }
