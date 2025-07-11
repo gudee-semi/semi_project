d76a4f9cdeb5ede7b96964684f50904521fafd94
Junha Kwon <junharoket@naver.com>
2025-07-11 15:04:22 +0900

    커밋
    커밋

---
+++ library_lms/src/main/java/com/hy/dto/qna/Attach.java
@@ -0,0 +1,16 @@
+package com.hy.dto.qna;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Attach {
+	private int qnaAttachId;
+	private int qnaId;
+	private String oriName;
+	private String reName;
+	private String path;
+}
---
+++ library_lms/src/main/java/com/hy/mapper/qna/QnaMapper.java
@@ -0,0 +1,20 @@
+package com.hy.mapper.qna;
+
+import java.util.List;
+
+import com.hy.dto.qna.Attach;
+import com.hy.dto.qna.Qna;
+
+public interface QnaMapper {
+	List<Qna> selectQnaList(Qna param);
+	
+	int selectQnaCount(Qna param);
+	
+	Qna selectQnaOne(int qnaNo);
+
+	Attach selectAttachByQnaNo(int qnoNo);
+	
+	Attach selectAttachByAttachNo(int attachNo);
+	
+	int insertQna(Qna param);
+}
